2f4704f6a2ad9dde750a26ca6eba06f9306ed14b
Tim Mocking <timmocking@gmail.com>
2024-02-07 11:57:12 +0100

    Add getMFIclusters for metaclustering based on MFI patterns

---
+++ R/getMFIclusters.R
@@ -0,0 +1,76 @@
+#' @title Label peaks in a numerical vector of MFIs across clusters
+#' 
+#' @description \code{labelPeaks} assesses the MFIs of a given marker across
+#' a range of clusters. The number of peaks (max 3) are detected and labeled
+#' as either "unimodal" (1 peak), "low/high" (2 peaks) or "low/medium/high".
+#' 
+#' @usage labelPeaks(values)
+#' 
+#' @param values Vector of MFIs for a marker across clusters
+#' 
+#' @return A named list with cutoffs and labels for all clusters
+#' 
+#' @importFrom pracma findpeaks
+#' @export
+labelPeaks <- function(values){
+  # Find peaks
+  dens <- density(values)
+  # Identify peak patterns (maximum 3 per channel)
+  peaks <- pracma::findpeaks(dens$y, npeaks = 3)
+  if (nrow(peaks) == 1){
+    cutoffs <- c()
+    labels <- rep("unimodal", length(values))
+  } else if (nrow(peaks) == 2){
+    cutoffs <- c(dens$x[peaks[1, 4]])
+    labels <- ifelse(values > cutoffs[1], "high", "low")
+  } else if (nrow(peaks) == 3){
+    cutoffs <- c(dens$x[peaks[1, 4]], dens$x[peaks[2, 4]])
+    labels <- cut(values, c(-Inf, cutoffs, Inf), labels = c("low", "medium", "high"),
+                  include.lowest = TRUE)
+  }
+  output <- list("cutoffs" = cutoffs,
+                 "labels" = labels)
+}
+
+
+#' @title Perform metaclustering based on MFI patterns across clusters
+#' 
+#' @description \code{getMFIclusters} assesses the MFIs of a given marker across
+#' a range of clusters. The number of peaks (max 3) are detected and labeled
+#' as either "unimodal" (1 peak), "low/high" (2 peaks) or "low/medium/high". Metaclusters
+#' are formed by aggregating patterns across all markers for all clusters.
+#' 
+#' @usage getMFIclusters(MFIData)
+#' 
+#' @param MFIData Matrix of MFIs of every marker (column) on every cluster (row)
+#' 
+#' @return A named list containing the patterns, cutoffs and metaclustering
+#' 
+#' @export
+getMFIclusters <- function(MFIData){
+  peakLabels <- data.frame(matrix(ncol = ncol(MFIData), nrow = nrow(MFIData)))
+  colnames(peakLabels) <- colnames(MFIData)
+  peakCutoffs <- list()
+  for (parameter in colnames(MFIData)){
+    peakData <- labelPeaks(MFIData[, parameter])
+    peakLabels[, parameter] <- peakData$labels
+    peakCutoffs[[parameter]] <- peakData$cutoffs
+  }
+  # Create a representation containing the marker and the peak (e.g., "CD4high")
+  markerStrings <- peakLabels
+  for (i in seq_along(markers)){
+    markerStrings[, i] <- paste0(markers[i], peakLabels[, i])
+  }
+  # Create a representation of all marker strings (e.g., "CD3high_CD4high_CD8low")
+  fullStrings <- apply(markerStrings, 1, function(row) paste(row, collapse = "_"))
+  # Combine the unique strings into metaclusters
+  metaclustering <- as.integer(factor(fullStrings, levels = unique(fullStrings)))
+
+  # Format final output
+  peakLabels$cluster <- rownames(peakLabels)
+  peakLabels$metacluster <- metaclustering
+  output <- list("data" = peakLabels,
+                 "cutoffs" = peakCutoffs,
+                 "metaclustering" = metaclustering)
+  return(output)
+}
--- README.md
+++ README.md
@@ -40,3 +40,25 @@ A number of automated doublet gates have been incorporated in cytometry software
 Suboptimal compensation introduces negative events which are problematic for default log-transformation. To solve this the logicle transformation uses a linear transformation for a range of the data. The width of this range can be estimated automatically using flowCore. 
 
 The *getLogicle* estimates the median width across a set of FCS files and returns a transformList object which can be used to transform a flowframe. 
+
+
+
+**Perform metaclustering based on cluster MFIs**
+
+The default consensus metaclustering of packages like FlowSOM is often suboptimal, and choices for metacluster counts are hard to determine. When using mostly lineage or cell type markers for clustering (e.g., CD3, CD4, CD8), it is relatively straightforward to separate populations into negative/dim/positive/bright subsets. 
+
+The *getMFIclusters* function can use the MFIs of such markers across N clusters. For each marker, a density estimation is performed, which is used to detect peaks of MFIs across clusters (e.g., negative / positive). This results in a string representation for every cluster (e.g., "CD3low"). These are combined across markers for every cluster (e.g., "CD3high/CD4low/CD8high") and all clusters are grouped together if they have a similar string, resulting in a metaclustering. 
+
+The benefit of this approach is that metaclusters have more unimodal marker distributions, while also being interpretable based on their label. 
+
+```{r}
+# Perform FlowSOM
+fSOM <- FlowSOM(ff, xdim = 20, ydim = 20) # Large grid for better density patterns
+MFIData <- GetClusterMFIs(fSOM, colsUsed = TRUE)
+
+# Get new metaclustering
+mc <- getMFIclusters(MFIData)$metaclustering
+
+# Replace existing metaclusters in FlowSOM
+fSOM <- UpdateMetaclusters(fSOM, clusterAssignment = mc)
+```
